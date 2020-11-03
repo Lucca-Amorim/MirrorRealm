0d99557c850c59b32e5a99712f73abd74f9ebcd1
Lucca Amorim <lucca.amorim12@gmail.com>
2020-11-02 21:09:33 -0300

    0.0.2
    - Menu Inicial adicionado
    - Menu Opcoes adicionado
    - Slider de Volume adicionado

---
+++ Mirror Realm/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class SceneLoader : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
