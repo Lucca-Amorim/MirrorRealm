1bcfee2b77708393aabb027444dc2de545fd171d
JhonSme <43253806+Jhonsme@users.noreply.github.com>
2020-11-17 22:38:43 -0300

    0.0.8

--- Mirror Realm/Assets/Scripts/offMap.cs
+++ Mirror Realm/Assets/Scripts/offMap.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class offMap : MonoBehaviour{
     
-
-
-
-
-
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("fazer o player morrer");
+        GameObject play = other.gameObject;
+        play.transform.position = new  Vector3(3.4f, 0.3f, 0);
     }
     
 }
---
+++ Mirror Realm/Assets/testeeee.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testeeee : MonoBehaviour{
+    Rigidbody2D corpo;
+
+    void Start(){
+        corpo = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.G)) corpo.AddForce(new Vector2(-222,0));
+        if(Input.GetKeyDown(KeyCode.H)) corpo.AddForce(new Vector2(222,0));
+    }
+}
